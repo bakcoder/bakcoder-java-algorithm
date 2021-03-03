9d87bbe6e12770b9a066e7e07619024330a290e8
bakcoder <gudwls45@gmail.com>
2021-03-03 12:35:40 +0900

    ADD: 2진수 출력, 배열의 합

--- src/AlgorithmMain.java
+++ src/AlgorithmMain.java
@@ -13,5 +13,15 @@ public class AlgorithmMain {
         System.out.println("\n3. 문자열 역프린트-------------");
         //3. 문자열 역프린트
         tester.getStringReversePrint("String");
-    } 
+
+        System.out.println("\n4. 이진수 출력-------------");
+        //4. 이진수 출력
+        tester.getBinValue(10);
+
+        System.out.println("\n5. 배열의 합-------------");
+        //5. 배열의 합
+        int[] arr = {1,2,3,4,5};
+        int sumResult = tester.getArrayValueSum(arr, arr.length);
+        System.out.println(sumResult);
+    }
 }
--- src/RecursiveAlgorithm.java
+++ src/RecursiveAlgorithm.java
@@ -28,4 +28,23 @@ public class RecursiveAlgorithm {
         }
     }
 
+    //4. 2진수 출력
+    public void getBinValue(int digit) {
+        if(digit < 2) {
+            System.out.print(digit);
+        }else {
+            getBinValue(Integer.valueOf(digit/2));
+            System.out.print(digit%2);
+        }
+    }
+
+    //5. 배열의 합 구하기
+    public int getArrayValueSum(int[] arr, int len) {
+        if(len == 0) {
+            return 0;
+        }else {
+            return arr[len-1] + getArrayValueSum(arr, len-1);
+        }
+    }
+
 }
