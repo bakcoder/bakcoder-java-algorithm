81f061582f246dacb63305f679714e10439d5e92
bakcoder <gudwls45@gmail.com>
2021-08-31 17:16:56 +0900

    sorting Algorithm : InsertionSort.java

---
+++ src/sort/InsertionSort.java
@@ -0,0 +1,25 @@
+package sort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        InsertionSort insertionSort = new InsertionSort();
+        int[] nums = {9,2,7,4,1};
+        System.out.println(Arrays.toString(insertionSort.sort(nums)));
+    }
+
+    public int[] sort(int[] nums) {
+
+        for(int i = 1 ; i < nums.length ; i++) {
+            for(int j = 0 ; j < i ; j++) {
+                if(nums[i] < nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
